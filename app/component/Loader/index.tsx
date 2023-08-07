92b26bb4fd55c57dcb09f870bee42510f1166445
Yogie Arifin <yogiearifinin@thebeam.co>
2023-08-08 01:22:26 +0700

    initial commit

---
+++ app/component/Loader/index.tsx
@@ -0,0 +1,8 @@
+export const Loader = () => {
+  return (
+    <div>
+      <div className="loader"></div>
+      <div><h1 className="text-3xl text-[#FFEE03]">Loading...</h1></div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,171 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { Loader } from "./component/Loader";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+type DeepDiveDataType = {
+  endTime: string;
+  startTime: string;
+  variants: {
+    biome: string;
+    name: string;
+    seed: number;
+    stages: {
+      anomaly: string | null;
+      id: number;
+      primary: string;
+      secondary: string;
+      warning: string | null;
+    }[];
+    type: string;
+  }[];
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+export default async function Home() {
+  const res = await fetch("https://drgapi.com/v1/deepdives");
+  const data: DeepDiveDataType = await res.json();
+  const renderIcon = () => {
+    return (
+      <Image
+        src={require("./media/deep-dive-icon.webp")}
+        alt="Deep Dive Icon"
+        className="mx-2 object-contain"
+        loading="lazy"
+      />
+    );
+  };
+
+  const converDate = (date: string) => {
+    return new Date(date).toString();
+  };
+
+  const renderMissionIcons = (mission: string) => {
+    if (mission.toLowerCase().includes("on-site refining")) {
+      return (
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={require("./media/missionIcons/liquid-morkite.webp")}
+          alt={mission}
+          width={24}
+          height={24}
         />
+      );
+    }
+    if (mission.toLowerCase().includes("egg")) {
+      return (
+        <Image
+          src={require("./media/missionIcons/egg.webp")}
+          alt={mission}
+          width={24}
+          height={24}
+        />
+      );
+    }
+    if (mission.toLowerCase().includes("morkite")) {
+      return (
+        <Image
+          src={require("./media/missionIcons/morkite.webp")}
+          alt={mission}
+          width={24}
+          height={24}
+        />
+      );
+    }
+    if (mission.toLowerCase().includes("dreadnought")) {
+      return (
+        <Image
+          src={require("./media/missionIcons/boss.webp")}
+          alt={mission}
+          width={24}
+          height={24}
+        />
+      );
+    }
+    if (mission.toLowerCase().includes("mule")) {
+      return (
+        <Image
+          src={require("./media/missionIcons/mule.webp")}
+          alt={mission}
+          width={24}
+          height={24}
+        />
+      );
+    }
+    if (mission.toLowerCase().includes("black box")) {
+      return (
+        <Image
+          src={require("./media/missionIcons/blackbox.webp")}
+          alt={mission}
+          width={24}
+          height={24}
+        />
+      );
+    }
+    if (mission.toLowerCase().includes("escort duty")) {
+      return (
+        <Image
+          src={require("./media/missionIcons/dotty.png")}
+          alt={mission}
+          width={24}
+          height={24}
+        />
+      );
+    }
+  };
+  return (
+    <main className="flex min-h-screen flex-col items-center flex-wrap p-4">
+      <div className="flex my-4">
+        {renderIcon()}
+        <div className="flex items-center">
+          <h1 className="text-[#FFEE03] text-xl text-center">
+            Deep Rock Galactic Deep Dive Tracker
+          </h1>
+        </div>
+        {renderIcon()}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {data ? (
+        <div>
+          <div>
+            <p className="text-lg text-center">
+              Start Time: {converDate(data.startTime)}
+            </p>
+            <p className="text-lg text-center text-red-500">
+              End Time: {converDate(data.endTime)}
+            </p>
+          </div>
+          {data.variants.map((item, index) => {
+            return (
+              <div key={index} className="p-2">
+                <h1 className="text-2xl">{item.type}</h1>
+                <h3>Name: {item.name}</h3>
+                <h3>Biome: {item.biome}</h3>
+                <div className="flex flex-wrap justify-between">
+                  {item.stages.map((items, index) => {
+                    return (
+                      <div key={index} className="m-4">
+                        <p>Stage {index + 1}</p>
+                        <div className="flex flex-wrap">
+                          <p className="mr-2">Primary: {items.primary}</p>
+                          {renderMissionIcons(items.primary)}
+                        </div>
+                        <div className="flex flex-wrap">
+                          <p className="mr-2">Secondary: {items.secondary}</p>
+                          {renderMissionIcons(items.secondary)}
+                        </div>
+                        <p className={items.anomaly ? "text-yellow-500" : ""}>
+                          Anomaly: {items.anomaly ?? "none"}
+                        </p>
+                        <p className={items.warning ? "text-red-500" : ""}>
+                          Warning: {items.warning ?? "none"}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <Loader />
+      )}
     </main>
-  )
+  );
 }
